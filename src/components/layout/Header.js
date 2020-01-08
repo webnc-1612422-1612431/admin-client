a7cc71155380b213323632413cb72aa70825bb6d
TranBaNgoc <1612431@student.hcmus.edu.vn>
2020-01-09 01:15:40 +0700

    Update Header.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,9 +3,21 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { bindActionCreators } from 'redux';
 
+const localStorage = require('localStorage');
+
 class Header extends React.Component {
+
+  constructor(props){
+    super(props);
+    const user = localStorage.getItem('user');
+    if (user === null) {
+      window.location.href = '/login';
+    }
+  }
+
   handleClickProfile = (e) => {
     e.preventDefault();
+    
     const {history}=this.props;
     history.push('/profile');
   }
